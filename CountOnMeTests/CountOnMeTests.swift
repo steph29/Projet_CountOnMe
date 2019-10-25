1badd2d636dd2af807e6eae526aff27f59bdf83b
steph29 <stephane.verardo@hotmail.fr>
2019-10-25 15:07:24 +0200

    Files for testing created

---
+++ CountOnMeTests/CountOnMeTests.swift
@@ -0,0 +1,17 @@
+//
+//  CountOnMeTests.swift
+//  CountOnMeTests
+//
+//  Created by stephane verardo on 25/10/2019.
+//  Copyright © 2019 Vincent Saluzzo. All rights reserved.
+//
+
+import XCTest
+@testable import CountOnMe
+
+class CountOnMeTests: XCTestCase {
+    func testExemple() {
+        XCTAssert(true)
+    }
+
+}
